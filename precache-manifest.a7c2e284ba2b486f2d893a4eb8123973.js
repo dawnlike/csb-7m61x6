90afe3352024b0d187ae0c4b201329ec7e54fac4
Code Sandbox <hello@codesandbox.io>
2023-01-06 18:56:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a7c2e284ba2b486f2d893a4eb8123973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7960b32a4b51376ca256283009d36a",
+    "url": "https://dawnlike.github.io/csb-7m61x6/index.html"
+  },
+  {
+    "revision": "4ae7e26e1a18e5a7b161",
+    "url": "https://dawnlike.github.io/csb-7m61x6/static/js/2.970564a0.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "https://dawnlike.github.io/csb-7m61x6/static/js/2.970564a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6b63f28fe7834b6b912",
+    "url": "https://dawnlike.github.io/csb-7m61x6/static/js/main.59bb606c.chunk.js"
+  },
+  {
+    "revision": "92525df356e542c7bd0e",
+    "url": "https://dawnlike.github.io/csb-7m61x6/static/js/runtime-main.0abe019d.js"
+  }
+]);
